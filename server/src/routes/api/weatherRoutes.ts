02937737a75bed3f2f15934a37dede2ebe61e9d0
Enk0-A <sergiyenkoyan@gmail.com>
2025-02-22 20:17:37 -0600

    API routes complete

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,53 @@
 import { Router, type Request, type Response } from 'express';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
+import { get } from 'http';
+import historyService from '../../service/historyService.js';
+
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
-
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
+  const { city } = req.body;
+
+  if (!city) {
+    return res.status(400).json({ error: 'City name is required' });
+  } 
+  try {
+    const weatherData = await WeatherService.getWeatherByCity(city);
+    await historyService.saveCity(city);
+    res.json(weatherData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to retrieve weather data or save city' });
+  } 
+  
   // TODO: GET weather data from city name
   // TODO: save city to search history
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get('/history', async (req: Request, res: Response) => {
+  try {
+    const history = await HistoryService.searchHistory();
+    res.json(history);
+  } catch (error) { 
+    console.error(error);
+    res.status(500).json({ error: 'Failed to retrieve search history' });
+
+  }
+});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+router.delete('/history/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    await HistoryService.removeCity(id);
+    res.status(204).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to delete history' })
+  }
+});
 
 export default router;
